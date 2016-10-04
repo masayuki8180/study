b6a747397f80e6c0f612c9d00a7534218cf50401
Yui901202 <oy003702@mb.taiyokogyo.co.jp>
2016-10-04 17:30:39 +0900

    10/04

--- Study/ViewController.swift
+++ Study/ViewController.swift
@@ -9,20 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test")
-        print("だよん")
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
